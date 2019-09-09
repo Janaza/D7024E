37c6443dda1c7402a842457a15d8c3660f099984
Jens Gertzell <jens.grt@gmail.com>
2019-09-09 19:33:13 +0200

    Fixed indentation

--- compose/compose.go
+++ compose/compose.go
@@ -1,21 +1,24 @@
 package compose
+
 import (
-"os"
-"os/exec"
-"log")
+	"log"
+	"os"
+	"os/exec"
+)
+
 func main() {
-cmd := exec.Command("docker", "swarm", "init")
-cmd2 := exec.Command("docker", "stack", "deploy", "-c", "docker-compose.yml", "d7024e")
-cmd.Stdout = os.Stdout
-cmd.Stderr = os.Stderr
-err := cmd.Run()
-if err != nil {
-log.Fatal(err)
-}
-cmd2.Stdout = os.Stdout
-cmd2.Stderr = os.Stderr
-err = cmd2.Run()
-if err != nil {
-        log.Fatal(err)
-}
+	cmd := exec.Command("docker", "swarm", "init")
+	cmd2 := exec.Command("docker", "stack", "deploy", "-c", "docker-compose.yml", "d7024e")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd2.Stdout = os.Stdout
+	cmd2.Stderr = os.Stderr
+	err = cmd2.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
