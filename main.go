a1a6a06a503c881dd5515a0953e08db9d85a7821
Unknown <jesgla-2@student.ltu.se>
2019-09-12 10:37:19 +0200

    automatic host ip
    The listener now automatically gets the hosts outbound ip

--- main.go
+++ main.go
@@ -14,14 +14,27 @@ func ErrorHandler(err error){
 		log.Fatal(err)
 	}
 }
-
+//get my outbound ip
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP
+}
 func Listener(){
 	fmt.Println("Server is starting...")
-	listenAdrs, _ := net.ResolveUDPAddr("udp", "localhost:10001")
+	myip := GetOutboundIP()
+	//ip must be of type string
+	myip2 := myip.String()
+	listenAdrs, _ := net.ResolveUDPAddr("udp", myip2+ ":10001")
 	servr, err := net.ListenUDP("udp", listenAdrs)
 	ErrorHandler(err)
 	defer servr.Close()
 	for{
+		fmt.Println(net.Interfaces())
 		fmt.Println("Listening on: " + string(listenAdrs.String()))
 		msgbuf := make([]byte, 1024)
 		n, adrs, err := servr.ReadFrom(msgbuf)
