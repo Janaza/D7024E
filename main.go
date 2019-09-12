240b7296e92c885dc93cb194e656f5299de3fe1d
Unknown <jesgla-2@student.ltu.se>
2019-09-12 11:24:49 +0200

    Get ip

--- main.go
+++ main.go
@@ -24,12 +24,53 @@ func GetOutboundIP() net.IP {
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	return localAddr.IP
 }
+
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", err
+}
+
 func Listener(){
 	fmt.Println("Server is starting...")
-	myip := GetOutboundIP()
+	hej, _ := externalIP()
+	fmt.Println(hej)
+	//myip := GetOutboundIP()
 	//ip must be of type string
-	myip2 := myip.String()
-	listenAdrs, _ := net.ResolveUDPAddr("udp", myip2+ ":10001")
+	//myip2 := myip.String()
+	listenAdrs, _ := net.ResolveUDPAddr("udp", hej+ ":10001")
 	servr, err := net.ListenUDP("udp", listenAdrs)
 	ErrorHandler(err)
 	defer servr.Close()
