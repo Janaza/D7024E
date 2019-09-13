28997f9e5db289c714a343ff809c8440add6f0d2
Jens Gertzell <jenger-5@student.ltu.se>
2019-09-13 14:13:53 +0200

    Update kademlia.go

--- kademlia.go
+++ kademlia.go
@@ -4,11 +4,11 @@ type Kademlia struct {
 }
 
 func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
+	// TODO FIND_NODE
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
-	// TODO
+	// TODO FIND_VALUE
 }
 
 func (kademlia *Kademlia) Store(data []byte) {
