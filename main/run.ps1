11378c7330f377795bd94a2e29174ef2fdfe8178
Jens Gertzell <jenger-5@student.ltu.se>
2019-09-19 20:00:02 +0200

    longer sleep

--- main/run.ps1
+++ main/run.ps1
@@ -3,9 +3,9 @@ $bootstrap = "localhost:1"
 go build .\main.go
 Start-Sleep -Seconds 2
 Start-Process powershell.exe -ArgumentList $file, "1"
-Start-Sleep -Milliseconds 50
+Start-Sleep -Milliseconds 100
 Start-Process powershell.exe -ArgumentList $file, "40", $bootstrap
 For ($i=2; $i -le 30; $i++) {
 Start-Process -NoNewWindow powershell.exe -ArgumentList $file, $i, $bootstrap
-Start-Sleep -Milliseconds 50
+Start-Sleep -Milliseconds 200
 }
