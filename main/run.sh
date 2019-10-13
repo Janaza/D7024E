9e1a48b81b11b4d2edd4a45aaeec036a0c8480a4
Jens Gertzell <jenger-5@student.ltu.se>
2019-10-13 14:15:41 +0200

    Update run.sh

--- main/run.sh
+++ main/run.sh
@@ -5,12 +5,12 @@ docker container rm $(docker container ls -aq)
 docker network rm d7024e_kademlia_network
 docker network create --ipam-driver=default --subnet=10.0.0.0/24 --gateway=10.0.0.1 d7024e_kademlia_network  
 
-docker run --net d7024e_kademlia_network --ip 10.0.0.2 --volume /home/d7024e-user/go/src/D7024E/:/go/src/D7024E --volume  /home/d7024e-user/go/src/D7024E/main/init/bootstrap/:/init --name bootstrap_node -t -d kadlab
+docker run --net d7024e_kademlia_network --ip 10.0.0.2 --volume /home/d7024e-user/go/src/D7024E/:/go/src/D7024E --volume  /home/d7024e-user/go/src/D7024E/main/init/bootstrap/:/init --name bootstrap_node -it -d kadlab
 port=6000
 for i in $(seq 1 50)
 do
     port=$((port+1))
     ip="10.0.0.$((i+2))"
-    docker run --net d7024e_kademlia_network --ip "$ip" --volume /home/d7024e-user/go/src/D7024E/:/go/src/D7024E --volume /home/d7024e-user/go/src/D7024E/main/init/node/:/init --name "node_$i" -t -d kadlab "$port"
+    docker run --net d7024e_kademlia_network --ip "$ip" --volume /home/d7024e-user/go/src/D7024E/:/go/src/D7024E --volume /home/d7024e-user/go/src/D7024E/main/init/node/:/init --name "node_$i" -it -d kadlab "$port"
 done
 
